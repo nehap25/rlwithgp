7a9abecddb57cdfd77228db28492aa9f7c63c285
Neha Prasad <nehaprasad@Nehas-MBP-2.home>
2020-03-22 12:29:12 -0400

    gp class

--- experiment.py
+++ experiment.py
@@ -2,6 +2,7 @@ from math import log
 import os
 import pybullet as p #DEPENDENCY
 import pybullet_data
+import numpy as np
 from numpy import float32
 from numpy import array
 from simple_world.constants import DIM, ORI, FRICTION, GRIPPER_ORI, GRIPPER_X, GRIPPER_Y, GRIPPER_Z, MASS, MAX_FRICTION, MAX_MASS, MIN_FRICTION, MIN_MASS, STEPS, WLH
@@ -16,12 +17,9 @@ full_pose, robot, objects,use_gui,goal_pose,mass_bool=setup(True)
 """
 Potential Questions for authors: 
 - how to define epsilon, delta, Ns, discount, Rmax in Experiment 1
+- GP Update Equations 
+- Reward function parameters 
 """
-
-
-#Supporting functions 
-def covering_number(states, r, dist_per_step):
-	return len(states)
 
 #Parameters to define 
 lipschitz = 9
@@ -59,6 +57,31 @@ for action in actions:
   print(reward)
 ########################
 
+class GP:
+
+	def __init__(self, means, kernel, states):
+		self.mean = {}
+		for i in range(len(states)):
+			self.mean[states[i]] = mean[i]
+		self.covar = kernel + noise_var*np.identity(len(states))
+		self.states = states
+
+	def update(self, state, reward_val):
+		self.mean[state] = inv(self.covar)
+
+
+	def mean(self, state):
+		return self.mean[state]
+
+	def variance(self, state):
+		state_ind = states.index(state)
+		return self.covar[state_ind][state_ind]
+
+
+#Supporting functions 
+def covering_number(states, r, dist_per_step):
+	return len(states)
+
 #Calculates distances between two states (based on L1 metric for experiment 1)
 def d(s, si):
 	distance = 0
@@ -89,19 +112,19 @@ def argmax_action(Q_dict, s_t):
 Q_dict = {}
 GP_actions = {}
 for action in actions:
-	GP_actions[action] = [Rmax/(1 - discount), rbf_kernel(states, states, gamma=1/(2*theta**2))]
+	means = [Rmax/(1 - discount) for i in range(len(states))]
+	GP_actions[action] = GP(means, rbf_kernel(states, states, gamma=1/(2*theta**2)), states)
 
 for t in timesteps:
 	a_t = argmax_action(Q_dict, s_t)
 	s_t = np.add(s_t, a_t)
 	r_t = #?
 	q_t = r_t + discount*max(Q(s_t, a, Q_dict) for a in actions)
-	s_t_index = states.index(s_t)
-	sigma_one_squared = GP_actions[a_t][1][s_t_index][s_t_index]
+	sigma_one_squared = GP_actions[a_t].variance(s_t)
 	if sigma_one_squared > var_threshold:
-		#GP_actions[a_t].update(s_t, q_t) (line 11)
-	sigma_two_squared = GP_actions[a_t][1][s_t_index][s_t_index]
-	#mean = GP_actions[a_t].mean(s_t) (line 13)
+		GP_actions[a_t].update(s_t, q_t) 
+	sigma_two_squared = GP_actions[a_t].variance(s_t)
+	mean = GP_actions[a_t].mean(s_t)
 	if sigma_one_squared > var_threshold and var_threshold >= sigma_two_squared and Q(s_t, a_t, Q_dict) - mean > 2*epsilon_one:
 		new_mean = mean + epsilon_one
 		selected_keys = []
@@ -112,5 +135,6 @@ for t in timesteps:
 			del Q_dict[key]
 		Q_dict[(s_t, a_t)] = new_mean
 		for action in actions:
-			#new_mu = Q_a (line 15)
-			GP_actions[action] = [, rbf_kernel(states, states, gamma=1/(2*theta**2))]
+			new_mu = [Q(s, action, Q_dict) for s in states]
+			GP_actions[action] = GP(new_mu, rbf_kernel(states, states, gamma=1/(2*theta**2)), states)
+
