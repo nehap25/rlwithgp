10499c43250b8f202b368df5c2977c7e09508c4d
Neha Prasad <nehaprasad@Nehas-MBP-2.home>
2020-04-07 10:36:32 -0400

    tom changes

--- experiment.py
+++ experiment.py
@@ -1,5 +1,3 @@
-
-
 from math import log
 import os
 import numpy as np
@@ -25,18 +23,9 @@ from sklearn.metrics.pairwise import rbf_kernel
 ########################################################
 
 
-"""
-Potential Questions for authors:
-- how to define epsilon, delta, Ns, discount, Rmax in Experiment 1
-- GP Update Equations
-- Reward function parameters
-"""
-
 # Parameters to define
 lipschitz = 9
 s_t = (0, 0)
-current = s_t
-dist_per_step = 0.1
 actions = [(0.1, 0), (-0.1, 0), (0, 0.1), (0, -0.1)]
 noise_var = 0.1
 timesteps = 10000
@@ -44,8 +33,8 @@ states = [(i/10, j/10) for i in range(0, 10) for j in range(0, 10)]
 rbf_theta = 0.05
 
 # Accuracy parameters--UNKNOWN
-epsilon = 0.3
-delta = 0.3
+epsilon = 99
+delta = 0.99
 
 # Reward function parameters
 discount = 0.99
@@ -55,87 +44,59 @@ Vmax = Rmax*timesteps
 ##################################################
 
 
-def covering_number(states, r, dist_per_step):
+def covering_number(r):
     area = math.pi*r*r
     return math.ceil(1/area)
 
 
-Ns = covering_number(states, epsilon*(1 - discount) /
-                     (3*lipschitz), dist_per_step)  # N_S (ε(1−γ)/(3lipschitz))
+Ns = covering_number(epsilon*(1 - discount)/(3*lipschitz))  # N_S (ε(1−γ)/(3lipschitz))
 
 
 # Sensitivity analysis of k, (sigma_tol)^2, e_1
-multiplicative_factor = epsilon*(1-discount)/(3*lipschitz)
-k = len(actions)*Ns*multiplicative_factor * \
-    ((3*Rmax/((1 - discount)**2)*epsilon) + 1)
+k = len(actions)*Ns*(3*Rmax/(((1 - discount)**2)*epsilon) + 1)
 var_threshold_num = 2*noise_var*(epsilon**2)*((1 - discount)**4)
 var_threshold_denom = 9*(Rmax**2)
-log_val = log(multiplicative_factor*len(actions)*Ns*(1 + k)*6/delta)
+log_val = log(len(actions)*Ns*(1 + k)*6/delta)
 var_threshold_denom *= log_val
 var_threshold = var_threshold_num/var_threshold_denom
 epsilon_one = epsilon*(1 - discount)/3
 
-
 class GP:
 
-    def __init__(self, means, kernel, states):
-        self.mean = {}
-        for i in range(len(states)):
-            self.mean[states[i]] = means[i]
+    def __init__(self, mean, kernel):
+        self.mean = mean
         self.kernel = kernel
-        self.covar = kernel(states, states, gamma=1/(2*(rbf_theta**2)))
-        self.states = states
         self.x_values = []
         self.y_values = []
 
-    def update(self, state, reward_val):
+    def update(self, state, reward_val): 
         self.x_values.append(state)
         self.y_values.append(reward_val)
 
-        new_state = np.array(list(state)).reshape(1, -1)
-
-        KXstate = self.kernel(self.x_values, new_state,
-                              gamma=1/(2*(rbf_theta**2)))
-        KXX = inv(self.kernel(self.x_values, self.x_values) +
-                  noise_var * np.eye(len(self.x_values)))
-        self.mean[state] = np.matmul(
-            np.matmul(KXstate.T, KXX), self.y_values)[0]
-
-        Kxx = self.kernel(new_state, new_state, gamma=1/(2*(rbf_theta**2)))
-        KXstate = self.kernel(self.x_values, new_state,
-                              gamma=1/(2*(rbf_theta**2)))
-        KXX = inv(self.kernel(self.x_values, self.x_values, gamma=1 /
-                              (2*(rbf_theta**2))) + noise_var*np.eye(len(self.x_values)))
-        new_variance = Kxx - np.matmul(np.matmul(KXstate.T, KXX), KXstate)
-
-        state_ind = self.states.index(state)
-        self.covar[state_ind][state_ind] = new_variance[0][0]
-
-    def mean_state(self, state):
-        # if self.x_values != []:
-        #     new_state = np.array(list(state)).reshape(1, -1)
-        #     KXstate = self.kernel(self.x_values, new_state,
-        #                           gamma=1/(2*(rbf_theta**2)))
-        #     KXX = inv(self.kernel(self.x_values, self.x_values) +
-        #               noise_var * np.eye(len(self.x_values)))
-        #     self.mean[state] = np.matmul(
-        #         np.matmul(KXstate.T, KXX), self.y_values)[0]
-        return self.mean[state]
+    def mean_state(self, state): 
+        new_mean = self.mean
+        if self.x_values != []:
+            new_state = np.array(list(state)).reshape(1, -1)
+            KXstate = self.kernel(np.array(self.x_values), new_state,
+                                  gamma=1/(2*(rbf_theta**2)))
+            KXX = inv(self.kernel(np.array(self.x_values), np.array(self.x_values), gamma=1/(2*(rbf_theta**2))) +
+                      noise_var * np.eye(len(self.x_values)))
+            new_mean = np.matmul(np.matmul(KXstate.T, KXX), np.array(self.y_values))[0]
+        return new_mean
 
     def variance(self, state):
-        state_ind = self.states.index(state)
-        # if self.x_values != []:
-        #     new_state = np.array(list(state)).reshape(1, -1)
-        #     Kxx = self.kernel(new_state, new_state, gamma=1/(2*(rbf_theta**2)))
-        #     KXstate = self.kernel(self.x_values, new_state,
-        #                           gamma=1/(2*(rbf_theta**2)))
-        #     KXX = inv(self.kernel(self.x_values, self.x_values, gamma=1 /
-        #                           (2*(rbf_theta**2))) + noise_var*np.eye(len(self.x_values)))
-        #     new_variance = Kxx - np.matmul(np.matmul(KXstate.T, KXX), KXstate)
-        #     self.covar[state_ind][state_ind] = new_variance[0][0]
-        return self.covar[state_ind][state_ind]
-
-# Supporting functions
+        new_state = np.array(list(state)).reshape(1, -1)
+        if self.x_values != []:
+            Kxx = self.kernel(new_state, new_state, gamma=1/(2*(rbf_theta**2)))
+            KXstate = self.kernel(np.array(self.x_values), new_state,
+                                  gamma=1/(2*(rbf_theta**2)))
+            KXX = inv(self.kernel(np.array(self.x_values), np.array(self.x_values), gamma=1 /
+                                  (2*(rbf_theta**2))) + noise_var*np.eye(len(self.x_values)))
+            new_variance = Kxx[0][0] - np.matmul(np.matmul(KXstate.T, KXX), KXstate)[0][0]
+            state_variance = new_variance
+        else:
+            state_variance = self.kernel(new_state, new_state, gamma=1/(2*(rbf_theta**2)))[0][0]
+        return state_variance
 
 
 # Calculates distances between two states (based on L1 metric for experiment 1)
@@ -146,8 +107,6 @@ def d(s, si):
     return distance
 
 # Equation 7
-
-
 def Q(s, a, Q_dict):
     currentMin = float('inf')
     for (si, ai) in Q_dict:
@@ -158,28 +117,31 @@ def Q(s, a, Q_dict):
     return min(currentMin, Vmax)
 
 # Line 6 of algorithm
-
-
-def argmax_action(Q_dict, s_t):
-    a = None
+def argmax_action(Q_dict, s_t, noise):
+    final_a = None
+    actual_a = None
     currentMax = float('-inf')
     for action in actions:
-        new_s = np.add(s_t, action).tolist()
-        new_s = tuple([round(x, 1) for x in new_s])
-        if new_s not in states:
+        new_a = action
+        if action[0] == 0:
+            new_a = (action[0], action[1] + noise)
+        else:
+            new_a = (action[0] + noise, action[1])
+        new_s = np.add(s_t, new_a).tolist()
+        new_s = tuple([round(x, 2) for x in new_s])
+        if new_s[0] < 0 or new_s[0] > 1 or new_s[1] < 0 or new_s[1] > 1:
             continue
         if Q(s_t, action, Q_dict) > currentMax:
             currentMax = Q(s_t, action, Q_dict)
-            a = action
-
-    return a
+            final_a = new_a
+            actual_a = action
+    return final_a, actual_a
 
 
 def get_reward_v1(s_t):
     if ((1-s_t[0])**2 + (1-s_t[1])**2)**0.5 > 0.15:
         return 0
     return 1,
-
 
 def get_reward_v2(s_t):
     return 2 - ((1-s_t[0])**2 + (1-s_t[1])**2)**0.5
@@ -188,22 +150,22 @@ def get_reward_v2(s_t):
 Q_dict = {}
 GP_actions = {}
 for action in actions:
-    means = [Rmax/(1 - discount) for i in range(len(states))]
-    GP_actions[action] = GP(means, rbf_kernel, states)
+    GP_actions[action] = GP(Rmax/(1 - discount), rbf_kernel)
 
 for t in range(timesteps):
-    a_t = argmax_action(Q_dict, s_t)
-    s_t = tuple(np.add(s_t, a_t).tolist())
-    s_t = tuple([round(i, 1) for i in s_t])
-    print(t, s_t)
+    noise = np.random.uniform(-0.01, 0.01)
+    final_a, actual_a = argmax_action(Q_dict, s_t, noise)
+    s_t = tuple(np.add(s_t, final_a).tolist())
+    s_t = tuple([round(i, 2) for i in s_t])
     r_t = get_reward_v2(s_t)
     q_t = r_t + discount*max(Q(s_t, a, Q_dict) for a in actions)
-    sigma_one_squared = GP_actions[a_t].variance(s_t)
+    sigma_one_squared = GP_actions[actual_a].variance(s_t)
     if sigma_one_squared > var_threshold:
-        GP_actions[a_t].update(s_t, q_t)
-    sigma_two_squared = GP_actions[a_t].variance(s_t)
-    print(sigma_two_squared)
-    if sigma_one_squared > var_threshold and var_threshold >= sigma_two_squared and Q(s_t, a_t, Q_dict) - a_t_mean > 2*epsilon_one:
+        GP_actions[actual_a].update(s_t, q_t)
+    sigma_two_squared = GP_actions[actual_a].variance(s_t)
+    a_t_mean = GP_actions[actual_a].mean_state(s_t)
+    print(t, s_t, sigma_two_squared)
+    if sigma_one_squared > var_threshold and var_threshold >= sigma_two_squared and Q(s_t, actual_a, Q_dict) - a_t_mean > 2*epsilon_one:
         new_mean = a_t_mean + epsilon_one
         selected_keys = []
         for (sj, aj) in Q_dict:
@@ -211,7 +173,7 @@ for t in range(timesteps):
                 selected_keys.append((sj, aj))
         for key in selected_keys:
             del Q_dict[key]
-        Q_dict[(s_t, a_t)] = new_mean
+        Q_dict[(s_t, actual_a)] = new_mean
         for action in actions:
             new_mu = [Q(s, action, Q_dict) for s in states]
-            GP_actions[action] = GP(new_mu, rbf_kernel, states)
+            GP_actions[action] = GP(new_mu, rbf_kernel)
