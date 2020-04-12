a46b0a7ba9f19b5a92b6b667fa3301e6f3f21a9e
Sarah Roden <soumya1910@gmail.com>
2020-04-12 11:50:52 -0400

    Update experiment.py

--- experiment.py
+++ experiment.py
@@ -1,155 +1,196 @@
 from math import log
 import os
-import pybullet as p #DEPENDENCY
-import pybullet_data
 import numpy as np
 from numpy import float32
 from numpy import array
 from numpy.linalg import inv
-from simple_world.constants import DIM, ORI, FRICTION, GRIPPER_ORI, GRIPPER_X, GRIPPER_Y, GRIPPER_Z, MASS, MAX_FRICTION, MAX_MASS, MIN_FRICTION, MIN_MASS, STEPS, WLH
-from simple_world.utils import Conf, Pose, Robot, close_enough, create_object, create_stack, eul_to_quat, get_full_aabb, get_pose, get_yaw, rejection_sample_aabb, rejection_sample_region, sample_aabb, set_conf, set_pose, step_simulation
-from simple_world.primitives import get_push_conf, move, push
-from gen_data import *
+
 import time
 import math
 from sklearn.metrics.pairwise import rbf_kernel
-full_pose, robot, objects,use_gui,goal_pose,mass_bool=setup(True)
-#change boolean to False to get rid of visualization
-"""
-Potential Questions for authors: 
-- how to define epsilon, delta, Ns, discount, Rmax in Experiment 1
-- GP Update Equations 
-- Reward function parameters 
-"""
 
-#Parameters to define 
+####CODE FOR ROBOTICS ENVIRONMENT--UNNECESSARY NOW#####
+#######################################################
+# import pybullet as p
+# import pybullet_data
+# from simple_world.constants import DIM, ORI, FRICTION, GRIPPER_ORI, GRIPPER_X, GRIPPER_Y, GRIPPER_Z, MASS, MAX_FRICTION, MAX_MASS, MIN_FRICTION, MIN_MASS, STEPS, WLH
+# from simple_world.utils import Conf, Pose, Robot, close_enough, create_object, create_stack, eul_to_quat, get_full_aabb, get_pose, get_yaw, rejection_sample_aabb, rejection_sample_region, sample_aabb, set_conf, set_pose, step_simulation
+# from simple_world.primitives import get_push_conf, move, push
+# from gen_data import *
+# full_pose, robot, objects,use_gui,goal_pose,mass_bool=setup(True)
+# for action in actions:
+#  robot,objects,use_gui,reward=step(action[0],action[1],robot,objects,goal_pose,use_gui)
+########################################################
+
+
+# Parameters to define
 lipschitz = 9
 s_t = (0, 0)
-current = s_t
-dist_per_step = 0.1
 actions = [(0.1, 0), (-0.1, 0), (0, 0.1), (0, -0.1)]
 noise_var = 0.1
 timesteps = 200
 states = [(i/10, j/10) for i in range(0, 10) for j in range(0, 10)]
 rbf_theta = 0.05
 
-#Accuracy parameters 
-epsilon = 
-delta = 
-Ns =  covering_number(states, epsilon*(1 - discount)/(3*lipschitz), dist_per_step)# N_S (ε(1−γ)/(3lipschitz))
+# Accuracy parameters--UNKNOWN
+epsilon = 1
+delta = 0.96
 
-#Reward function parameters 
-discount = 0.99
-Rmax =
-Vmax = 
+# Reward function parameters
+discount = 0.02
+####CHANGE BELOW IF YOU CHANGE REWARD FUNCTION####
+Rmax = 2
+Vmax = Rmax*timesteps
+##################################################
 
-#Sensitivity analysis of k, (sigma_tol)^2, e_1
-k = len(A)*Ns*((3*Rmax/((1 - discount)**2)*epsilon) + 1)
+
+def covering_number(r):
+    area = math.pi*r*r
+    return math.ceil(1/area)
+
+
+Ns = covering_number(epsilon*(1 - discount)/(3*lipschitz))  # N_S (ε(1−γ)/(3lipschitz))
+
+k = len(actions)*Ns*(3*Rmax/(((1 - discount)**2)*epsilon) + 1)
 var_threshold_num = 2*noise_var*(epsilon**2)*((1 - discount)**4)
-var_threshold_denom = 9*(Rmax**2)*log(len(A)*Ns*(1 + k)*6/delta)
+var_threshold_denom = 9*(Rmax**2)
+log_val = log(len(actions)*Ns*(1 + k)*6/delta)
+var_threshold_denom *= log_val
 var_threshold = var_threshold_num/var_threshold_denom
 epsilon_one = epsilon*(1 - discount)/3
 
-
-##CODE FOR ONE EPISODE##
-full_pose, robot, objects,use_gui,goal_pose,mass_bool=setup(True)
-for action in actions:
-  robot,objects,use_gui,reward=step(action[0],action[1],robot,objects,goal_pose,use_gui)
-  print(reward)
-########################
-
 class GP:
 
-	def __init__(self, means, kernel, states):
-		self.mean = {}
-		for i in range(len(states)):
-			self.mean[states[i]] = mean[i]
-		self.kernel = kernel
-		self.covar = kernel(states, states, gamma=1/(2*(rbf_theta**2)))
-		self.states = states
+    def __init__(self, mean, kernel, action, Q_dict={}):
+        self.mean = mean
+        self.kernel = kernel
+        self.action = action
+        self.Q_dict = Q_dict
+        self.x_values = []
+        self.y_values = []
 
-	def update(self, state, reward_val):
-		K = self.kernel(state, state, gamma=1/(2*(rbf_theta**2))) + noise_var
-		K_s = self.kernel(state, self.states, gamma=1/(2*(rbf_theta**2))) 
-		K_ss = self.kernel(self.states, self.states, gamma=(1/(2*(rbf_theta**2)))) + noise_var*np.eye(len(self.states))
-		K_inv = inv(K)
-		#should K_inv = inv(K_ss)?--> i think that's what both equations are taking the inverse of?
-		mu_s = K_s.T.dot(K_inv).dot([reward_val])
-		for i in range(len(states)):
-			self.mean[states[i]] = mu_s[i]
-		#should K_ss be K?--> the equation says k(x',x') - <something>
-		self.covar = K_ss - K_s.T.dot(K_inv).dot(K_s)
+    def update(self, state, reward_val): 
+        self.x_values.append(state)
+        self.y_values.append(reward_val)
 
-	def mean(self, state):
-		return self.mean[state]
+    def mean_state(self, state): 
+        new_mean = self.mean
+        if self.mean == "Q_MEAN":
+            new_mean = Q(state, self.action, self.Q_dict)
+        if self.x_values != []:
+            new_state = np.array(list(state)).reshape(1, -1)
+            KXstate = self.kernel(np.array(self.x_values), new_state,
+                                  gamma=1/(2*(rbf_theta**2)))
+            KXX = inv(self.kernel(np.array(self.x_values), np.array(self.x_values), gamma=1/(2*(rbf_theta**2))) +
+                      noise_var * np.eye(len(self.x_values)))
+            new_mean = np.matmul(np.matmul(KXstate.T, KXX), np.array(self.y_values))[0]
+        return new_mean
 
-	def variance(self, state):
-		state_ind = states.index(state)
-		return self.covar[state_ind][state_ind]
+    def variance(self, state):
+        new_state = np.array(list(state)).reshape(1, -1)
+        if self.x_values != []:
+            Kxx = self.kernel(new_state, new_state, gamma=1/(2*(rbf_theta**2)))
+            KXstate = self.kernel(np.array(self.x_values), new_state,
+                                  gamma=1/(2*(rbf_theta**2)))
+            KXX = inv(self.kernel(np.array(self.x_values), np.array(self.x_values), gamma=1 /
+                                  (2*(rbf_theta**2))) + noise_var*np.eye(len(self.x_values)))
+            new_variance = Kxx[0][0] - np.matmul(np.matmul(KXstate.T, KXX), KXstate)[0][0]
+            state_variance = new_variance
+        else:
+            state_variance = self.kernel(new_state, new_state, gamma=1/(2*(rbf_theta**2)))[0][0]
+        return state_variance
 
 
-#Supporting functions 
-
-def covering_number(states, r, dist_per_step):
-	return len(states)
-
-#Calculates distances between two states (based on L1 metric for experiment 1)
+# Calculates distances between two states (based on L1 metric for experiment 1)
 def d(s, si):
-	distance = 0
-	for i in range(len(s)):
-		distance += abs(s[i] - si[i])
-	return distance
+    distance = 0
+    for i in range(len(s)):
+        distance += abs(s[i] - si[i])
+    return distance
 
-#Equation 7
+# Equation 7
 def Q(s, a, Q_dict):
-	currentMin = float('inf')
-	for (si, ai) in Q_dict:
-		if ai == a:
-			mu = Q_dict[(si, ai)]
-			total = mu + lipschitz*d(s, si)
-			currentMin = min(currentMin, total)
-	return min(currentMin, Vmax)
+    currentMin = float('inf')
+    for (si, ai) in Q_dict:
+        if ai == a:
+            mu = Q_dict[(si, ai)]
+            total = mu + lipschitz*d(s, si)
+            currentMin = min(currentMin, total)
+    return min(currentMin, Vmax)
 
-#Line 6 of algorithm 
-def argmax_action(Q_dict, s_t):
-	a= None
-	currentMax = float('-inf')
-	for action in actions:
-		if Q(s_t, action, Q_dict) > currentMax:
-			currentMax = Q(s_t, action, Q_dict)
-			a = action
-	return a
-def get_reward(s_t):
-	if ((1-s_t[0])**2 + (1-s_t[1])**2)**0.5 > 0.15:
-		return 0
-	return 1
+# Line 6 of algorithm
+def argmax_action(Q_dict, s_t, noise):
+    final_a = None
+    actual_a = None
+    currentMax = float('-inf')
+    for action in actions:
+        new_a = action
+        if action[0] == 0:
+            new_a = (action[0], action[1] + noise)
+        else:
+            new_a = (action[0] + noise, action[1])
+        new_s = np.add(s_t, new_a).tolist()
+        new_s = tuple([round(x, 3) for x in new_s])
+        if new_s[0] < 0 or new_s[0] > 1 or new_s[1] < 0 or new_s[1] > 1:
+            continue
+        if Q(s_t, action, Q_dict) > currentMax:
+            currentMax = Q(s_t, action, Q_dict)
+            final_a = new_a
+            actual_a = action
+    return final_a, actual_a
+
+
+def get_reward_v1(s_t):
+    if ((1-s_t[0])**2 + (1-s_t[1])**2)**0.5 > 0.15:
+        return 0
+    return 1,
+
+def get_reward_v2(s_t):
+    return 2 - ((1-s_t[0])**2 + (1-s_t[1])**2)**0.5
+
 
 Q_dict = {}
 GP_actions = {}
 for action in actions:
-	means = [Rmax/(1 - discount) for i in range(len(states))]
-	GP_actions[action] = GP(means, rbf_kernel, states)
+    GP_actions[action] = GP(Rmax/(1 - discount), rbf_kernel, action)
 
-for t in timesteps:
-	a_t = argmax_action(Q_dict, s_t)
-	s_t = np.add(s_t, a_t)
-	r_t = get_reward(s_t) #RESOLVE:unclear if reward is sparse or not!!
-	q_t = r_t + discount*max(Q(s_t, a, Q_dict) for a in actions)
-	sigma_one_squared = GP_actions[a_t].variance(s_t)
-	if sigma_one_squared > var_threshold:
-		GP_actions[a_t].update(s_t, q_t) 
-	sigma_two_squared = GP_actions[a_t].variance(s_t)
-	mean = GP_actions[a_t].mean(s_t)
-	if sigma_one_squared > var_threshold and var_threshold >= sigma_two_squared and Q(s_t, a_t, Q_dict) - mean > 2*epsilon_one:
-		new_mean = mean + epsilon_one
-		selected_keys = []
-		for (sj, aj) in Q_dict:
-			if new_mean + lipschitz*d(sj, s_t) <= Q_dict[(sj, aj)]:
-				selected_keys.append((sj, aj))
-		for key in selected_keys:
-			del Q_dict[key]
-		Q_dict[(s_t, a_t)] = new_mean
-		for action in actions:
-			new_mu = [Q(s, action, Q_dict) for s in states]
-			GP_actions[action] = GP(new_mu, rbf_kernel, states)
+episodes=300
+avg_reward=[]
+for i in range(episodes):
+    s_t=(0,0)
+
+    episode_rewards=0
+    print(i,Q_dict)
+    for t in range(timesteps):
+        noise = np.random.normal(0, 0.01)
+        final_a, actual_a = argmax_action(Q_dict, s_t, noise)
+        s_t = tuple(np.add(s_t, final_a).tolist())
+        s_t = tuple([round(i, 2) for i in s_t])
+        r_t = get_reward_v2(s_t)
+        episode_rewards+=r_t
+        q_t = r_t + discount*max(Q(s_t, a, Q_dict) for a in actions)
+        sigma_one_squared = GP_actions[actual_a].variance(s_t)
+        if sigma_one_squared > var_threshold:
+            if (s_t,actual_a) in Q_dict:
+              GP_actions[actual_a].update(s_t, q_t - Q_dict[(s_t,actual_a)])
+            else:
+              GP_actions[actual_a].update(s_t, q_t)
+        sigma_two_squared = GP_actions[actual_a].variance(s_t)
+        a_t_mean = GP_actions[actual_a].mean_state(s_t)
+        if sigma_one_squared > var_threshold and var_threshold >= sigma_two_squared and Q(s_t, actual_a, Q_dict) - a_t_mean > 2*epsilon_one:
+            new_mean = a_t_mean + epsilon_one
+            selected_keys = []
+            for (sj, aj) in Q_dict:
+                if new_mean + lipschitz*d(sj, s_t) <= Q_dict[(sj, aj)]:
+                    selected_keys.append((sj, aj))
+            for key in selected_keys:
+                del Q_dict[key]
+            if (s_t,actual_a) in Q_dict:
+              Q_dict[(s_t, actual_a)] = Q_dict[(s_t, actual_a)] + new_mean
+            else:
+              Q_dict[(s_t, actual_a)] =  new_mean
+            for action in actions:
+                GP_actions[action] = GP("Q_MEAN", rbf_kernel, action, Q_dict)
+    avg_reward.append(episode_rewards)
+    print(avg_reward)
 
